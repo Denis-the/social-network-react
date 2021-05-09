369311935881ab723bd02f982e1c290bcd404d72
Denis-the <d.rozumnyu@gmail.com>
2021-05-09 23:02:06 +0300

    added 404-redirect

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import Preloader from './components/common/Preloader/Preloader';
 import store from './redux/redux-store';
+import NotFound from './components/404-Not-found/404';
 
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainerWithHooks'));
 const UsersContainer = React.lazy(() => import('./components/Users/UsersContainerWithHooks'));
-const Dialogs = React.lazy(() => import('./components/Dialogs/Dialogs.js'));
+const Dialogs = React.lazy(() => import('./components/Dialogs/Dialogs'));
+const SettingsContainer = React.lazy(() => import('./components/Settings/SettingsContainer'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -30,10 +32,13 @@ class App extends React.Component {
           <div className="app-content__wrapper">
             <Suspense fallback={<><div>loading...</div><Preloader/></>}>
               <Switch>
+                <Route path='/' exact component={ProfileContainer} />
                 <Route path='/profile/:userId?' component={ProfileContainer} />
                 <Route path='/dialogs' render={() => <Dialogs/>} />
                 <Route path='/users' render={() => <UsersContainer />} />
+                <Route path='/settings' render={() => <SettingsContainer />} />
                 <Route path='/login' render={() => <Login />} />
+                <Route render={() => <NotFound />} />
               </Switch>
             </Suspense>
             
@@ -54,7 +59,7 @@ const mapStateToProps = state => ({
 export const AppContainer = connect(mapStateToProps, { initilizeApp })(App);
 
 const MainApp = () => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <AppContainer />
     </Provider>
